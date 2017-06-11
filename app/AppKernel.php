e1275bdc2d3c4570677a7acbe4a99a6ba6736961
petrero <petrero@gmail.com>
2017-06-11 08:58:45 +0300

    Copy code from branch 8

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -13,6 +13,10 @@ class AppKernel extends Kernel {
       new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
       new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
       new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
+        
+      new Knp\Bundle\MarkdownBundle\KnpMarkdownBundle(), 
+      new Doctrine\Bundle\DoctrineCacheBundle\DoctrineCacheBundle(),  
+        
       new AppBundle\AppBundle(),
     );
 
